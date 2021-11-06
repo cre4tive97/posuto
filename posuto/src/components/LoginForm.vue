ea5ebc4ea95d8806063a8daae448944c57f45aae
cre4tive97 <harryborrison5148@gmail.com>
2021-11-06 20:41:10 +0900

    login api done

--- posuto/src/components/LoginForm.vue
+++ posuto/src/components/LoginForm.vue
@@ -1,19 +1,37 @@
 <template>
-  <form class="form">
+  <form class="form" @submit.prevent="submitForm">
     <div class="form-box">
       <label for="username">Username</label>
-      <input class="input" id="username" type="text" />
+      <input class="input" id="username" type="text" v-model="username" />
     </div>
     <div class="form-box">
-      <label for="username">Password</label>
-      <input class="input" id="username" type="text" />
+      <label for="password">Password</label>
+      <input class="input" id="password" type="text" v-model="password" />
     </div>
     <button class="btn">Login</button>
   </form>
 </template>
 
 <script>
-export default {};
+import { loginUser } from '@/api/auth';
+export default {
+  name: 'LoginForm',
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    async submitForm() {
+      const { data } = await loginUser({
+        username: this.username,
+        password: this.password,
+      });
+      console.log(data);
+    },
+  },
+};
 </script>
 
 <style scoped></style>
