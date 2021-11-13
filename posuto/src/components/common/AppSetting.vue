23366af80102f2313f01b5ee66c4fe45b03ca9fe
cre4tive97 <harryborrison5148@gmail.com>
2021-11-13 21:05:15 +0900

    app setting color markup

--- posuto/src/components/common/AppSetting.vue
+++ posuto/src/components/common/AppSetting.vue
@@ -1,13 +1,37 @@
 <template>
   <div class="setting">
-    <div class="greeting">
-      <h2>{{ $store.state.nickname }}</h2>
+    <div class="color">
+      <h1 class="color__title">Color</h1>
+      <div class="color__section">
+        <div class="color__btn" v-for="(color, i) in colors" :key="i">
+          <div
+            class="color__circle"
+            :style="{ backgroundColor: color.color }"
+          ></div>
+          <p style="font-size: 0.8rem">{{ color.name }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      colors: [
+        { color: '#FEC0CA', name: 'Pink' },
+        { color: '#ff7fff', name: 'Purple' },
+        { color: '#96d5ff', name: 'Skyblue' },
+        { color: '#f2f486', name: 'Yellow' },
+        { color: '#5ec4ff', name: 'Blue' },
+        { color: '#8cff9b', name: 'Green' },
+        { color: '#DAC6AE', name: 'Beige' },
+        { color: '#ff7d3d', name: 'Orange' },
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
@@ -17,12 +41,42 @@ export default {};
   right: 4rem;
   border-radius: 1rem;
   padding: 8px;
-  background-color: rgba(94, 154, 198, 0.3);
-  /* border: 1px solid rgba(62, 138, 193, 0.3); */
+  background-color: rgba(42, 128, 170, 0.4);
+  /* background-color: rgba(0, 0, 0, 0.2); */
   backdrop-filter: blur(16px);
   width: 50%;
   max-width: 400px;
   min-width: 250px;
-  height: 200px;
+  color: white;
+}
+h1 {
+  font-size: 1.5rem;
+}
+
+.color__section {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  width: 100%;
+}
+.color__btn {
+  display: flex;
+  justify-content: center;
+  padding: 4px 8px;
+  width: 80px;
+  background-color: rgba(42, 128, 170, 0.6);
+  border-radius: 8px;
+  margin-bottom: 0.4rem;
+  transition: background-color 0.3s;
+}
+.color__btn:hover {
+  cursor: pointer;
+  background-color: rgba(42, 128, 170, 1);
+}
+.color__circle {
+  width: 1rem;
+  height: 1rem;
+  border-radius: 50%;
+  margin-right: 8px;
 }
 </style>
