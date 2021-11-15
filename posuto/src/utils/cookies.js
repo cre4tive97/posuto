6642af015b2b39a4a1cf4e183001c52a5ecc8c99
cre4tive97 <harryborrison5148@gmail.com>
2021-11-15 22:29:24 +0900

    cookies manage with vue-cookies library

--- posuto/src/utils/cookies.js
+++ posuto/src/utils/cookies.js
@@ -1,27 +1,34 @@
+import VueCookies from 'vue-cookies';
+
 function saveAuthToCookie(value) {
-  document.cookie = `posuto_auth=${value}`;
+  // document.cookie = `posuto_auth=${value}`;
+  VueCookies.set('posuto_auth', value);
 }
 
 function saveUserToCookie(value) {
-  document.cookie = `posuto_user=${value}`;
+  // document.cookie = `posuto_user=${value}`;
+  VueCookies.set('posuto_user', value);
 }
 
 function getAuthFromCookie() {
-  return document.cookie.replace(
-    /(?:(?:^|.*;\s*)posuto_auth\s*=\s*([^;]*).*$)|^.*$/,
-    '$1',
-  );
+  // return document.cookie.replace(
+  //   /(?:(?:^|.*;\s*)posuto_auth\s*=\s*([^;]*).*$)|^.*$/,
+  //   '$1',
+  // );
+  VueCookies.get('posuto_auth');
 }
 
 function getUserFromCookie() {
-  return document.cookie.replace(
-    /(?:(?:^|.*;\s*)posuto_user\s*=\s*([^;]*).*$)|^.*$/,
-    '$1',
-  );
+  // return document.cookie.replace(
+  //   /(?:(?:^|.*;\s*)posuto_user\s*=\s*([^;]*).*$)|^.*$/,
+  //   '$1',
+  // );
+  VueCookies.get('posuto_user');
 }
 
 function deleteCookie(value) {
-  document.cookie = `${value}=; expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+  // document.cookie = `${value}=; expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+  VueCookies.remove(value);
 }
 
 export {
